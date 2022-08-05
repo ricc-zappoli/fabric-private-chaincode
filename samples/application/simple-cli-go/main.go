711814d582e3a9670b82860982e40bcb72032f64
Riccardo Zappoli <riccardo.zappoli@unifr.ch>
2022-08-05 19:19:29 +0200

    Refactor for EGo v1.0.0

--- samples/application/simple-cli-go/main.go
+++ samples/application/simple-cli-go/main.go
@@ -8,22 +8,26 @@ SPDX-License-Identifier: Apache-2.0
 package main
 
 import (
+	"encoding/csv"
+	"os"
+	"time"
+
 	"github.com/hyperledger/fabric-private-chaincode/samples/application/simple-cli-go/cmd"
 )
 
-const FileName = "goHW.csv"
+const FileName = "goSIM.csv"
 
 func main() {
-	//start := time.Now()
+	start := time.Now()
 	cmd.Execute()
-	//end := time.Since(start)
+	end := time.Since(start)
 
-	// csvFile, _ := os.OpenFile(FileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	// w := csv.NewWriter(csvFile)
-	// w.Write(append([]string{
-	// 	time.Now().UTC().Format("2006-01-02 15:04:05.000"),
-	// 	end.String(),
-	// }, os.Args[1:]...))
-	// w.Flush()
+	csvFile, _ := os.OpenFile(FileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	w := csv.NewWriter(csvFile)
+	w.Write(append([]string{
+		time.Now().UTC().Format("2006-01-02 15:04:05.000"),
+		end.String(),
+	}, os.Args[1:]...))
+	w.Flush()
 
 }
